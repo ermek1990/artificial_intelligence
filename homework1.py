f4ff225c4ad49f709c9902d720934c0fc5a4566b
Yermek Zhanabayev <e.zhanabayev@gmail.com>
2016-09-13 02:56:24 -0700

    Update homework1.py

--- homework1.py
+++ homework1.py
@@ -1,9 +1,33 @@
 from collections import OrderedDict
 
 
+# Used this class from stackoverflow to create custom OrderedDict with behavior of defaultdict
+class CustomDict(OrderedDict):
+    def __missing__(self, k):
+        self[k] = []
+        return self[k]
+
+
+class NodeQueue:
+    def __init__(self):
+        self.nodes = []
+
+    def put(self, node):
+        self.nodes.insert(0, node)
+
+    def get(self):
+        return self.nodes.pop()
+
+    def empty(self):
+        return not self.nodes
+
+    def ext(self, node):
+        self.nodes.extend(node)
+
+
 def func_is_empty_file():
     try:
-        file_obj = open('input3.txt')
+        file_obj = open(file_name)
         try:
             input_data = file_obj.read()
         finally:
@@ -51,13 +75,6 @@ def func_create_traffic_line_list(file_obj, line_count):
     return traffic_list
 
 
-# Used this class from stackoverflow to create custom ordered dictionary with behavior of default dictionary
-class CustomDict(OrderedDict):
-    def __missing__(self, i):
-        self[i] = []
-        return self[i]
-
-
 def func_create_node_dict(line_list):
     nodes = CustomDict()
     for i, node in enumerate(line_list):
@@ -65,41 +82,11 @@ def func_create_node_dict(line_list):
     return nodes
 
 
-# That's what I've used before
-# def func_create_node_dict(line_list):
-#     from collections import defaultdict
-#     nodes = defaultdict()
-#     for i, node in enumerate(line_list):
-#         nodes[node[0]].append(node[1])
-#     return nodes
-
-
-class NodeQueue:
-    def __init__(self):
-        self.nodes = []
-
-    def put(self, node):
-        self.nodes.insert(0, node)
-
-    def get(self):
-        return self.nodes.pop()
-
-    def empty(self):
-        return not self.nodes
-
-
-class NodeStack:
-    def __init__(self):
-        self.nodes = []
-
-    def put(self, node):
-        self.nodes.extend(node)
-
-    def get(self):
-        return self.nodes.pop()
-
-    def empty(self):
-        return not self.nodes
+def func_create_node_dict_astar(line_list):
+    nodes = CustomDict()
+    for i, node in enumerate(line_list):
+        nodes[node[0]].append(node[1])
+    return nodes
 
 
 def func_bfs(nodes, start_node, goal_node):
@@ -128,8 +115,8 @@ def func_dfs(nodes, start_node, goal_node):
     for node_keys in nodes.keys():
         nodes[node_keys].reverse()
     nodes_visited = []
-    nodes_stack = NodeStack()
-    nodes_stack.put(start_node)
+    nodes_stack = NodeQueue()
+    nodes_stack.ext(start_node)
     while not nodes_stack == []:
         node = nodes_stack.get()
         if node == goal_node:
@@ -137,8 +124,7 @@ def func_dfs(nodes, start_node, goal_node):
             break
         if node not in nodes_visited:
             nodes_visited = nodes_visited + [node]
-            # nodes[node].reverse()
-            nodes_stack.put(n for n in nodes[node] if n not in nodes_visited)
+            nodes_stack.ext(n for n in nodes[node] if n not in nodes_visited)
     output = ''
     for i in range(0, len(nodes_visited)):
         output += nodes_visited[i] + ' ' + str(i) + '\n'
@@ -146,37 +132,62 @@ def func_dfs(nodes, start_node, goal_node):
 
 
 def func_astar(nodes, start_node, goal_node):
-    return ''
+    output = ''
+    path_memo, nodes_visited, node_q = [], [start_node], NodeQueue()
+    node_q.put(nodes_visited)
+    while not node_q.empty():
+        nodes_visited = node_q.get()
+        prev_node = nodes_visited[len(nodes_visited) - 1]
+        if prev_node == goal_node:
+            path_memo.append(nodes_visited)
+        for node in nodes[prev_node]:
+            if node not in nodes_visited:
+                path = nodes_visited + [node]
+                node_q.put(path)
+    for i in range(0, len(path_memo)):
+        if len(path_memo[i]) == min(map(len, path_memo)):
+            for j in range(0, len(path_memo[i])):
+                output += path_memo[i][j] + ' ' + str(j) + '\n'
+            break
+    return output
 
 
 def func_ucs(nodes, start_node, goal_node):
     return ''
 
-
+file_name = 'input.txt'
 if not func_is_empty_file():
-    func_print_file_content('input3.txt')
-    file_inst = open('input3.txt', 'rU')
+    func_print_file_content(file_name)
+    file_inst = open(file_name, 'rU')
     algo = func_get_line_from_file(file_inst, 1).rstrip()
     start_state = func_get_line_from_file(file_inst, 1).rstrip()
     goal_state = func_get_line_from_file(file_inst, 1).rstrip()
     output_data = ''
     if start_state == goal_state:
         output_data = start_state + ' 0'
-    line_count = func_get_line_from_file(file_inst, 1).rstrip()
-    line_list = func_create_traffic_line_list(file_inst, int(line_count))
-    print(line_list)
-    node_dict = func_create_node_dict(line_list)
-    print(node_dict)
-    if algo == 'BFS':
-        output_data = func_bfs(node_dict, start_state, goal_state)
-    elif algo == 'DFS':
-        output_data = func_dfs(node_dict, start_state, goal_state)
-    elif algo == 'A*':
-        func_astar(node_dict, start_state, goal_state)
-    elif algo == 'UCS':
-        func_ucs(node_dict, start_state, goal_state)
     else:
-        print('The algorithm in the input file is incorrect.')
+        line_list = func_create_traffic_line_list(file_inst, int(func_get_line_from_file(file_inst, 1).rstrip()))
+        print(line_list)
+        if algo == 'BFS':
+            node_dict = func_create_node_dict(line_list)
+            print(node_dict)
+            output_data = func_bfs(node_dict, start_state, goal_state)
+        elif algo == 'DFS':
+            node_dict = func_create_node_dict(line_list)
+            print(node_dict)
+            output_data = func_dfs(node_dict, start_state, goal_state)
+        elif algo == 'A*':
+            node_dict = func_create_node_dict(line_list)
+            print(node_dict)
+            sun_line_list = func_create_traffic_line_list(file_inst, int(func_get_line_from_file(file_inst, 1).rstrip()))
+            print(sun_line_list)
+            # output_data = func_astar(node_dict, start_state, goal_state)
+        elif algo == 'UCS':
+            node_dict = func_create_node_dict(line_list)
+            print(node_dict)
+            func_ucs(node_dict, start_state, goal_state)
+        else:
+            print('The algorithm in the input file is incorrect.')
     if output_data != '':
         func_write_file(output_data)
         func_print_file_content('output.txt')
